2ef663ca2d95844196a8baa5b7ed81e05811872c
unknown <sfm009@gmail.com>
2022-08-17 13:51:02 +0300

    animals

--- tasks/easy/abstract_class/animals.py
+++ tasks/easy/abstract_class/animals.py
@@ -12,3 +12,36 @@ says таким образом, чтобы он возвращал строку 
 - "{кличка} - собака. Говорит ГАВ!" для класса Dog
 - "{кличка} - корова. Говорит МУ!" для класса Cow
 """
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    name: str
+
+    def __init__(self, name):
+        self.name = name
+
+    @abstractmethod
+    def says(self):
+        pass
+
+
+class Cat(Animal):
+
+    def says(self):
+        return f"{self.name} - кошка. Говорит МЯУ!"
+
+
+class Dog(Animal):
+
+    def says(self):
+        return f"{self.name} - собака. Говорит ГАВ!"
+
+
+class Cow(Animal):
+
+    def says(self):
+        return f"{self.name} - корова. Говорит МУ!"
+
+
+pet_1 = Animal
