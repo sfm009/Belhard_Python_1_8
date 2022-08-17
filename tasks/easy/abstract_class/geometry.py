eaf6b8672af0835c3760f47b128341131ed91faa
unknown <sfm009@gmail.com>
2022-08-17 17:00:55 +0300

    geometry

--- tasks/easy/abstract_class/geometry.py
+++ tasks/easy/abstract_class/geometry.py
@@ -28,3 +28,56 @@
 """
 from abc import ABC, abstractmethod
 from math import pi
+
+
+class Shape(ABC):
+    @abstractmethod
+    def get_perimeter(self):
+        pass
+
+    @abstractmethod
+    def get_square(self):
+        pass
+
+
+class Circle(Shape):
+    r: float
+
+    def __init__(self, r):
+        self.r = r
+
+    def get_perimeter(self):
+        return 2 * pi * self.r
+
+    def get_square(self):
+        return pi * (self.r ** 2)
+
+
+class Rectangle(Shape):
+    a: float
+    b: float
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def get_perimeter(self):
+        return 2 * (self.a + self.b)
+
+    def get_square(self):
+        return self.a * self.b
+
+
+class Square(Rectangle):
+
+    def __init__(self, a):
+        super().__init__(a)
+
+    def get_perimeter(self):
+        return self.a * 4
+
+    def get_square(self):
+        return self.a * self.a
+
+
+
