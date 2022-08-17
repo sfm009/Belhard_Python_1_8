f36b5d797fcb21a432abd5a94b51265efe7e9158
unknown <sfm009@gmail.com>
2022-08-18 02:23:23 +0300

    duck_typing

--- tasks/easy/inheritance_polimorphism/duck_typing.py
+++ tasks/easy/inheritance_polimorphism/duck_typing.py
@@ -19,3 +19,30 @@ i_love_science. Функция должна возвращать строку в
 
 https://www.youtube.com/watch?v=8o7ZKTvZpLc
 """
+
+
+class AmericanPerson:
+    def i_love_science(self):
+        return f"I love science"
+
+class RussianPerson:
+    def i_love_science(self):
+        return f"Я люблю науку"
+
+class GermanyPerson:
+    def i_love_science(self):
+        return f"ich liebe Wissenschaft"
+
+
+def person_love_science(obj):
+    obj.i_love_science()
+    return f"{obj.__class__.__name__} says that: {obj.i_love_science()}"
+
+
+if __name__ == "__main__":
+    obj_1 = AmericanPerson()
+    obj_2 = RussianPerson()
+    obj_3 = GermanyPerson()
+    person_love_science(obj_1)
+    person_love_science(obj_2)
+    person_love_science(obj_3)
